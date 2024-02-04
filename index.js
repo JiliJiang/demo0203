a30fc43a8cfa40dda8df108468bf17532cebd9c5
JiliJiang <jiangjili@gmail.com>
2024-02-03 19:18:33 -0800

    code in tutoring session

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿
+document.getElementById('saveBtn').onclick = function(){
+    
+    var userName = document.getElementById('userName').value;
+
+    var userText = document.getElementById('userText').value;
+
+    console.log(userName, userText);
+
+    localStorage.setItem('userName', userName);
+    localStorage.setItem('userText', userText);
+
+    window.location.href = 'index.html';
+}
